1cacaa5a7edb86f68d34426c2b839628fc08a3a6
Cliff <Ockiyacliffc@gmail.com>
2024-05-22 19:46:28 +0100

    initial commit

---
+++ app/index.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { View, ScrollView, SafeAreaView, Text} from "react-native";
+import { Stack, useRouter } from "expo-router";
+
+import { COLORS, icons, images, SIZES } from "../constants";
+import { Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome } from "../components";
+
+
+const Home = () => {
+    const router = useRouter();
+    return (
+        <SafeAreaView styles={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen 
+                
+            />
+        </SafeAreaView>
+    )
+}
+
+export default Home;
