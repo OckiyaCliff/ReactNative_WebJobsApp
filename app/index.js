b6283b7d7cb1c9d80c5f576920075bdd2d3ae4a0
Cliff <Ockiyacliffc@gmail.com>
2024-05-22 21:54:05 +0100

    navbar update

--- app/index.js
+++ app/index.js
@@ -11,8 +11,32 @@ const Home = () => {
     return (
         <SafeAreaView styles={{ flex: 1, backgroundColor: COLORS.lightWhite}}>
             <Stack.Screen 
-                
+                options={{
+                    headerStyle: { backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,
+                    headerLeft: () => (
+                        <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%"  />
+                    ), 
+                    headerRight: () => (
+                        <ScreenHeaderBtn iconUrl={images.profile} dimension="100%"  />
+                    ),
+                    headerTitle: "Hello"
+                }}
             />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View 
+                    style={{
+                        flex: 1,
+                        padding: SIZES.medium
+                    }}
+                >
+                    <Welcome />
+
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
         </SafeAreaView>
     )
 }
